7a37eec11e42668eac022905f8878e5013cb0db0
SdQL <sadavid2007@gmail.com>
2024-02-23 19:14:40 -0500

    Solving viral advertising problem with JS

---
+++ 11.viral-advertising/viral-advertising.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'viralAdvertising' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function viralAdvertising(n) {
+    let likedPeople = Math.floor(5/2)
+    let amount = 0
+    let day = 1
+    while(day < n) {
+       let shared = likedPeople * 3
+       likedPeople = Math.floor(shared / 2)
+       amount += likedPeople
+       
+       day++ 
+    }
+    
+    return amount + 2
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = viralAdvertising(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
